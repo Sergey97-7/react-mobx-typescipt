ba0e183e1d97de9901d496efa9e4e765e9abe084
sergey97-7 <sergey97-7@yandex.ru>
2019-07-01 11:47:51 +0300

    deploy

---
+++ precache-manifest.e3b5e7b633b46aa7cd6b69dd032dae9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd30e2a20adec879d673def10613b96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40aa754ed694d8e39aa6",
+    "url": "/static/css/main.54273ceb.chunk.css"
+  },
+  {
+    "revision": "961a18ea1fa5b4e12634",
+    "url": "/static/js/2.69e1dd5e.chunk.js"
+  },
+  {
+    "revision": "40aa754ed694d8e39aa6",
+    "url": "/static/js/main.c83cb759.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/stores/index.ts
+++
@@ -1,7 +0,0 @@
-import { ContentStore } from "./contentStore";
-import { OptionsPanelStore } from "./optionsPanelStore";
-
-export const stores = {
-  contentStore: new ContentStore(),
-  optionsPanelStore: new OptionsPanelStore()
-};
